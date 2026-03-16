799434ae83ca385bb1038d398d218c4c6630421f
Josh Bingham <joshuabingham@hotmail.co.uk>
2026-03-16 16:40:50 +0000

    Edit app.jsx

---
+++ frontend/First Step Jobs/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import JobSearch from "./JobSearch";
+
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>First Step Jobs</h1>
+      <JobSearch />
+    </div>
+  );
+}
+
+export default App;
