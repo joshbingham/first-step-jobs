5c25709b8adb9e521c806b6f5ae2d34bc2abc079
Josh Bingham <joshuabingham@hotmail.co.uk>
2026-04-01 14:01:30 +0100

    Create pure module and remove old getDistance function from server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import axios from "axios";
 import dotenv from "dotenv";
+import { getDistance } from "./utils/distance.js";
 
 dotenv.config();
 
@@ -10,22 +11,7 @@ app.use(cors());
 
 const PORT = 5000;
 
-// 📏 Haversine formula (distance in km)
-function getDistance(lat1, lon1, lat2, lon2) {
-  const R = 6371;
-  const dLat = (lat2 - lat1) * (Math.PI / 180);
-  const dLon = (lon2 - lon1) * (Math.PI / 180);
 
-  const a =
-    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.cos(lat1 * (Math.PI / 180)) *
-      Math.cos(lat2 * (Math.PI / 180)) *
-      Math.sin(dLon / 2) *
-      Math.sin(dLon / 2);
-
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  return R * c;
-}
 
 async function getCommuteTime(originLat, originLon, destLat, destLon) {
   if (!process.env.GOOGLE_MAPS_API_KEY) {
